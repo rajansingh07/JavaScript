bb587228d28492393e8ccfea981d148339cb3781
Rajan Kumar <168678961+rajansingh07@users.noreply.github.com>
2026-09-01 23:57:45 +0530

    Truthy && Falsy values

---
+++ Truthy.js
@@ -0,0 +1,15 @@
+const userEmail = false
+
+if (userEmail) {
+    console.log("I got the user email")
+} else {
+    console.log("Not got the user email")
+}
+
+// Falsy values
+
+// false, 0,  -0, BigInt 0n, "", null, undefined, NaN
+
+// Truthy values
+
+// [], "0", 'false', " " ,{}, function(){},
