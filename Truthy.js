d757964b600076e37561b6c52f48421a7496e0fd
Rajan Kumar <168678961+rajansingh07@users.noreply.github.com>
2026-09-02 00:52:50 +0530

    Updated Day 15 and Truthy & Falsy values

--- Truthy.js
+++ Truthy.js
@@ -1,4 +1,4 @@
-const userEmail = false
+const usersEmail = false
 
 if (userEmail) {
     console.log("I got the user email")
@@ -13,3 +13,46 @@ if (userEmail) {
 // Truthy values
 
 // [], "0", 'false', " " ,{}, function(){},
+
+const userEmail = "Rajan.com"
+
+// if (userEmail) {
+//     console.log("I got the user email")
+// } else {
+//     console.log("Not got the user email")
+// }
+
+// Falsy values
+
+// false, 0,  -0, BigInt 0n, "", null, undefined, NaN
+
+// Truthy values
+
+// [], "0", 'false', " " ,{}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty")
+// }
+
+const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("Object is empty")
+// }
+
+
+// Nullish Coalescing Operator (??): Null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+
+console.log(val1)
+
+// Condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80")  : console.log("grater than 80")
+
