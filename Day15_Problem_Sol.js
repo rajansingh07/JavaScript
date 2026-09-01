5e934120f635f3b81e2bcc5b42ce2168a14f99a4
rajansingh07 <sunaharasuman2004@gmail.com>
2026-09-02 03:04:51 +0530

    Control flow problem solving

---
+++ Day15_Problem_Sol.js
@@ -0,0 +1,257 @@
+// Positive, Negative, or Zero
+
+// let n = 0;
+
+// if (n < 0) {
+//     console.log("Negative")
+// } else if (n === 0) {
+//     console.log("Zero")
+// } else if (n > 0){
+//     console.log("Positive")
+// } else {
+//     console.log("Invalid")
+// }
+
+// Even or Odd
+
+// let n = 6
+
+// if (n % 2 === 0) {
+//     console.log("Even")
+// } else {
+//     console.log("Odd")
+// }
+
+// Voting Eligibility
+
+// let age = 10
+
+// if (age === 18) {
+//     console.log("Eligible to vote")
+// } else {
+//     console.log("Not eligible to vote")
+// }
+
+// Grater Number
+
+// let a = 13
+// let b = 10
+
+// if (a > b) {
+//     console.log("a is grater than b")
+// } else if (a === b){
+//     console.log("both are equal")
+// } else {
+//     console.log("a is lesser")
+// }
+
+// Fail Or Pass 
+
+// let marks = 100;
+
+// switch (true) {
+//     case marks >= 0 && marks <= 30:
+//         console.log("Fail");
+//         break;
+
+//     case marks >= 31 && marks <= 40:
+//         console.log("Pass");
+//         break;
+
+//     case marks >= 41 && marks <= 50:
+//         console.log("Grade C");
+//         break;
+
+//     case marks >= 51 && marks <= 60:
+//         console.log("Grade B");
+//         break;
+
+//     case marks >= 61 && marks <= 70:
+//         console.log("Grade A");
+//         break;
+
+//     case marks >= 71 && marks <= 80:
+//         console.log("Grade A");
+//         break;
+
+//     case marks >= 81 && marks <= 90:
+//         console.log("Grade A+");
+//         break;
+
+//     case marks >= 91 && marks <= 100:
+//         console.log("Full Marks");
+//         break;
+
+//     default:
+//         console.log("Invalid marks please enter valid marks");
+//         break;
+// }
+
+// Temprature Calculator
+
+// let temp = 30
+
+// switch (true) {
+//     case temp >= 0 && temp <= 10:
+//     console.log("Very Cold");
+//     break;
+//     case temp >= 11 && temp <= 20:
+//     console.log("Cold");
+//     break;
+//     case temp >= 21 && temp <= 30:
+//     console.log("Normal");
+//     break;
+//     case temp >= 31 && temp <= 40:
+//     console.log("Hot");
+//     break;
+//     case temp >= 41 && temp <= 50:
+//     console.log("Very Hot");
+//     break;
+// }
+
+// Find the Largest of Three Numbers
+
+// let a = 20;
+// let b = 15;
+// let c = 20;
+
+// switch (true) {
+//     case a > b:
+//         console.log("A is Bigger than B");
+//         break;
+
+//     case a === b:
+//         console.log("A & B are equal");
+//         break;
+
+//     case a < b:
+//         console.log("A is lesser than B");
+//         break;
+// }
+
+// switch (true) {
+//     case b > c:
+//         console.log("B is Bigger than C");
+//         break;
+
+//     case b === c:
+//         console.log("B & C are equal");
+//         break;
+
+//     case b < c:
+//         console.log("B is lesser than C");
+//         break;
+// }
+
+// switch (true) {
+//     case c > a:
+//         console.log("C is Bigger than A");
+//         break;
+
+//     case c === a:
+//         console.log("C & A are equal");
+//         break;
+
+//     case c < a:
+//         console.log("C is lesser than A");
+//         break;
+// }
+
+// Discount Calculator 
+
+// let isPurchasedAmount = 2000
+
+// if (isPurchasedAmount === 1000) {
+//     console.log("You are not ealigble for discount")
+// } else if (isPurchasedAmount === 2000) {
+//     console.log("You got 15% discount")
+// } else if (isPurchasedAmount >= 4000 && isPurchasedAmount <= 5000) {
+//     console.log("You got 18% discount")
+// } else if (isPurchasedAmount >= 5001 && isPurchasedAmount <= 7000) {
+//     console.log("You got 20% discount")
+// } else if (isPurchasedAmount >= 7001 && isPurchasedAmount <= 9999) {
+//     console.log("You got 25% discount")
+// } else if (isPurchasedAmount === 10000) {
+//     console.log("You got 30% discount")
+// } else {
+//     console.log("No discount");
+// }
+
+// Login Check
+
+// let username = "Rajan"
+// let password = "Rajan@123"
+
+// if (username === "Rajan" && password === "Rajan@123") {
+//     console.log(`welcome back ${username}`)
+// } else if (username !== "Rajan") {
+//     console.log("Username is incorrect")
+// } else if (password !== "Rajan@123") {
+//     console.log("password is incorrect")
+// } else {
+//     console.log("Invalid credentials")
+// }
+
+// Placed the order
+
+// let isUserloggedIn = true
+// let isPaymentMethod = true
+
+// if (isUserloggedIn && isPaymentMethod) {
+//     console.log("User eligble to place the order")
+// } else if (isUserloggedIn === false || isPaymentMethod === false ) {
+//     console.log("Not eligble to place order")
+// } else {
+//     console.log("Invalid User")
+// }
+
+// Free Delivery 
+
+// let purchasedAmount = 429
+// let permiumPass = false
+// let eligblityAmount = 500
+
+// if (purchasedAmount >= 500 || permiumPass) {
+//     console.log("Eigble for free delivery")
+// } else if (purchasedAmount <= 500) {
+//     let remainingAmount = eligblityAmount - purchasedAmount
+//     console.log(`Add ₹${remainingAmount} more to get free delivery, or purchase a premium pass.`)
+// }
+
+// Traffic light
+
+// let trafficLight = "Green"
+
+// switch(trafficLight){
+//     case "Red":
+//         console.log("Stop")
+//     break;
+
+//     case "Yellow":
+//         console.log("Ready to go")
+//     break;
+
+//     case "Green":
+//         console.log("Go")
+//     break;
+// } 
+
+// Menu 
+
+// let menu = 4;
+
+// switch (menu) {
+//     case 1:
+//         console.log("Home")
+//     break;
+//     case 2:
+//         console.log("Profile")
+//     break;
+//     case 3:
+//         console.log("Setting")
+//     break;
+//     case 4:
+//         console.log("Logout")
+//     break;
+// }
+
