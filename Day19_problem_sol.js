535f826a8eb024d3948142194a6317cc05aa87f5
rajansingh07 <sunaharasuman2004@gmail.com>
2026-09-08 11:04:29 +0530

    Solve huge amount of question based on filter, map and forEach

--- Day19_problem_sol.js
+++ Day19_problem_sol.js
@@ -63,4 +63,97 @@ const users = [
 
 const userName = users.filter( (user) => user.age > 18 && user.active === true)
 
-console.log(userName)
+// console.log(userName)
+
+
+const fullNums = [4, 12, 7, 20, 3, 15, 9, 25]
+
+const gratestNum = fullNums.filter( (num) => num > 10)
+
+// console.log(gratestNum);
+
+const wholeNums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const oddNumber = wholeNums.filter( (num) => {
+    return num % 2 != 0
+})
+
+// console.log(oddNumber)
+
+const allUsers = [
+  { name: "Rajan", age: 21 },
+  { name: "Aman", age: 17 },
+  { name: "Rahul", age: 25 },
+  { name: "Vikas", age: 16 }
+]
+
+const selectedUsers = allUsers.filter( (user) => user.age > 18)
+
+// console.log(selectedUsers)
+
+const fourNums = [5, 10, 15, 20]
+
+const tenNums = fourNums.map( (num) => num + 10)
+
+// console.log(tenNums)
+
+const prices = [100, 200, 300, 400]
+
+const productPrice = prices.map( (num) => num + 50)
+// console.log(productPrice)
+
+
+// Get only user name
+
+const websiteUsers = [
+  { name: "Rajan", age: 21 },
+  { name: "Aman", age: 17 },
+  { name: "Rahul", age: 25 }
+]
+
+const userNames = websiteUsers.map( (user) => user.name)
+
+// console.log(userNames)
+
+const products = [
+  { name: "Mouse", price: 300 },
+  { name: "Keyboard", price: 800 },
+  { name: "Monitor", price: 1200 },
+  { name: "USB", price: 200 }
+]
+
+const productPricing = products.filter( (products) => products.price > 500)
+
+// console.log(productPricing)
+
+// Get username
+
+const actUsers = [
+  { name: "Rajan", active: true },
+  { name: "Aman", active: false },
+  { name: "Rahul", active: true },
+  { name: "Vikas", active: false }
+]
+
+const activeUser = actUsers.filter( (user) => user.active)
+
+// console.log(activeUser)
+
+const chainingNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const magNums = chainingNums.map( (num) => num * 2)
+                    .map( (num) => num + 2)
+                    .filter( (num) => num > 15)
+
+// console.log(magNums)
+
+const Wlusers = [
+  { name: "Rajan", age: 21, active: true },
+  { name: "Aman", age: 17, active: true },
+  { name: "Rahul", age: 25, active: false },
+  { name: "Vikas", age: 22, active: true }
+]
+
+const filterUser = Wlusers.filter( (user) => user.active && user.age > 18)
+
+console.log(filterUser)
